ec89b6c8cf02794bdf1f902c29993384a1605dc2
jazeelwayanad <76432669+jazeelwayanad@users.noreply.github.com>
2024-05-06 12:31:51 +0000

    f

--- script.js
+++ script.js
@@ -1,53 +1,20 @@
-const counters = document.querySelectorAll(".counters span");
-const container = document.querySelector(".counters");
-// Variable that tracks if the counters have been activated
-let activated = false;
+$(document).ready(function(){
+    var a = 0;
+    $(window).scroll(function() {
+      var oTop = $('.counters').offset().top - window.innerHeight;
 
-//Add scroll event to the page
-window.addEventListener("scroll", () => {
-    /* If the page is scrolled to the containers
-    element and t ne counters are not voted */
-    if (
-        pageYOffset > container.offsetTop - container.offsetHeight - 5
-        && activated === false
-    ) {
-        counters.forEach(counter => {
-
-            counter.innerText = 0;
-
-            let count = 0;
-            //Update count function
-            function updateCount() {
-                //Get counter target number to count to
-                const target = parseInt(counter.dataset.count);
-                //As Long as the count is below the target number
-                if(count < target) {
-                    //Increment the count
-                    count++;
-                    //Set the counter text to the count
-                    counter.innerText = count;
-                    // Repeat this every 10 miliseconds
-                    setTimeout(updateCount, 30);
-                } else{
-                    counter.innerText = target;
-                }
+      if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.count').each(function() {
+          var $this = $(this);
+          jQuery({ Counter: 0 }).animate({ Counter: $this.attr('data-to-value') }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function () {
+              $this.text(Math.ceil(this.Counter));
             }
-
-            updateCount();
-
-            activated = true;
+          });
         });
-    } else if(
-        pageYOffset < container.offsetTop - container.offsetHeight - 100
-        || pageYOffset === 0
-        && activated === true
-    ) { 
-    //Select all counters
-    counters.forEach(counter =>{
-    //Set counter text back to zero 
-    counter.innerText = 0;
+        a = 1;
+      }
     });
-    //Set activated to false
-    activated = false;
-    }
-    });
+  });
